ac3423f3a3c3e7954589fa8386f48747d51e51fd
likhithabaimegavath2008-sudo <likhithabaimegavath2008@gmail.com>
2026-08-02 21:58:04 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Function call
+result = add(num1, num2)
+
+# Display result
+print("Sum =", result)
